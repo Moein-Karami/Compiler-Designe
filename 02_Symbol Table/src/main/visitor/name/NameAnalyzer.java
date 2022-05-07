552ddb43c33d0a07fd9f9a4b368faec3bd13ed41
Moein Karami <moein2000n@gmail.com>
2022-05-07 19:09:56 +0430

    checker analyze done

--- 02_Symbol Table/src/main/visitor/name/NameAnalyzer.java
+++ 02_Symbol Table/src/main/visitor/name/NameAnalyzer.java
@@ -15,286 +15,53 @@ import main.ast.nodes.statement.set.*;
 import main.compileError.CompileError;
 import main.symbolTable.SymbolTable;
 import main.symbolTable.exceptions.ItemAlreadyExistsException;
+import main.symbolTable.exceptions.ItemNotFoundException;
 import main.symbolTable.items.*;
 import main.visitor.*;
+import main.symbolTable.utils.graph.Graph;
+import main.symbolTable.utils.graph.exceptions.GraphDoesNotContainNodeException;
+import main.symbolTable.utils.graph.exceptions.NodeAlreadyExistsException;
 
 import java.util.HashMap;
 
-public class RandomString {
-    // function to generate a random string of length n
-    static String getAlphaNumericString(int n)
-    {
-        // chose a Character random from this String
-        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                + "0123456789"
-                + "abcdefghijklmnopqrstuvxyz";
-
-        // create StringBuffer size of AlphaNumericString
-        StringBuilder sb = new StringBuilder(n);
-        for (int i = 0; i < n; i++) {
-
-            // generate a random number between
-            // 0 to AlphaNumericString variable length
-            int index
-                    = (int)(AlphaNumericString.length()
-                    * Math.random());
-
-            // add Character one by one in end of sb
-            sb.append(AlphaNumericString
-                    .charAt(index));
-        }
-        return sb.toString();
-    }
-}
-
-
 public class NameAnalyzer extends Visitor<Void> {
 
-    public boolean has_error = false;
+    private Graph<String> par_pattern;
 
-    public static Stack<SymbolTable> symbol_stack = new Stack<>();
+    private Program program;
 
-    private Map<String, ClassSymbolTableItem> class_symbol_map = new HashMap<>();
+    public NameAnalyzer(Program program){
+        this.program = program;
+    }
 
-    @Override
-    public Void visit(Program program) {
-        SymbolTable.root = new SymbolTable();
-        symbol_stack.push(SymbolTable.root);
+    public void analyze(){
+        this.program.accept(new NameCollector());
+        this.set_par_pattern();
+        this.program.accept(new NameChecker(par_pattern));
+    }
 
-        ArrayList<VariableDeclaration> global_variables = program.getGlobalVariables();
-        ArrayList<ClassDeclaration> classes = program.getClasses();
+    private void set_par_pattern() {
+        Graph<String> par_pattern = new Graph<>();
+        for (ClassDeclaration cls : this.program.getClasses()){
+            String class_name = cls.getClassName().getName();
+            try {
+                par_pattern.addNode(class_name);
+            } catch (NodeAlreadyExistsException err) {}
 
-        for (VariableDeclaration var : global_variables)
-        {
-            GlobalVariableSymbolTableItem var_item = new GlobalVariableSymbolTableItem(var);
-            try
-            {
-                SymbolTable.root.put(var_item);
-            }
-            catch (ItemAlreadyExistsException err)
-            {
-                this.has_error = true;
-                var.addError(new CompileError(var.getLine(), ": Redefinition of global variable " + var_item.getName()));
-                var.setVarName(new Identifier(RandomString.getAlphaNumericString(25)));
-                SymbolTable.root.put(var_item);
-            }
+            if (cls.getParentClassName() == null)
+                continue;
+
+            String par_name = cls.getParentClassName().getName();
+
+            try {
+                par_pattern.addNodeAsParentOf(class_name, par_name);
+                ClassSymbolTableItem parent_item = (ClassSymbolTableItem) SymbolTable.root.getItem(ClassSymbolTableItem.START_KEY + par_name, true);
+                ClassSymbolTableItem cls_item = (ClassSymbolTableItem) SymbolTable.root.getItem(ClassSymbolTableItem.START_KEY + class_name, true);
+                cls_item.getClassSymbolTable().pre = parent_item.getClassSymbolTable();
+            }catch (ItemNotFoundException | GraphDoesNotContainNodeException err) {}
+            this.par_pattern = par_pattern;
         }
-
-        for (ClassDeclaration cls : classes)
-        {
-            ClassSymbolTableItem cls_item = new ClassSymbolTableItem(cls);
-            try
-            {
-                SymbolTable.root.put(cls_item);
-            }
-            catch (ItemAlreadyExistsException err)
-            {
-                cls.setClassName(new Identifier(RandomString.getAlphaNumericString(25)));
-                SymbolTable.root.put(cls_item);
-            }
-            class_symbol_map.put(cls.getClassName().getName(), cls_item);
-        }
-
-        for (ClassDeclaration cls : classes)
-        {
-            if (cls.getParentClassName() != null)
-            {
-                ClassSymbolTableItem parent_cls_item = class_symbol_map.get(cls.getParentClassName().getName());
-                ClassSymbolTableItem cur_cls_item = class_symbol_map.get(cls.getClassName().getName());
-                cur_cls_item.classSymbolTable.pre = parent_cls_item.classSymbolTable;
-            }
-        }
-
-        for (ClassDeclaration cls: classes)
-        {
-            cls.accept(this);
-        }
-        return null;
-    }
-    @Override
-    public Void visit(ClassDeclaration classDeclaration) {
-        ClassSymbolTableItem cls = new ClassSymbolTableItem(classDeclaration);
-
-        return null;
     }
 
-    @Override
-    public Void visit(ConstructorDeclaration constructorDeclaration) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(MethodDeclaration methodDeclaration) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(FieldDeclaration fieldDeclaration) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(VariableDeclaration varDeclaration) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(AssignmentStmt assignmentStmt) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(BlockStmt blockStmt) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(ConditionalStmt conditionalStmt) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(ElsifStmt elsifStmt) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(MethodCallStmt methodCallStmt) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(PrintStmt print) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(ReturnStmt returnStmt) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(EachStmt eachStmt) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(BinaryExpression binaryExpression) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(UnaryExpression unaryExpression) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(TernaryExpression ternaryExpression) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(ObjectMemberAccess objectOrListMemberAccess) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(Identifier identifier) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(ArrayAccessByIndex arrayAccessByIndex) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(MethodCall methodCall) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(NewClassInstance newClassInstance) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(SelfClass selfClass) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(NullValue nullValue) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(IntValue intValue) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(BoolValue boolValue) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(SetInclude setAdd) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(SetValue setValue) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(SetNew setMerge) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(SetDelete setDelete) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(SetMerge setAdd) {
-        //todo
-        return null;
-    }
-
-    @Override
-    public Void visit(RangeExpression rangeExpression) {
-        //todo
-        return null;
-    }
+    public Graph<String> get_par_pattern() {return par_pattern;}
 }
