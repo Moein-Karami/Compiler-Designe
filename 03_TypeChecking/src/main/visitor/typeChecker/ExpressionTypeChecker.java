0e88e4893c735c0406e1182d20e92eb90bcd5cb0
Moein Karami <moein2000n@gmail.com>
2022-05-25 20:34:05 +0430

    object done

--- 03_TypeChecking/src/main/visitor/typeChecker/ExpressionTypeChecker.java
+++ 03_TypeChecking/src/main/visitor/typeChecker/ExpressionTypeChecker.java
@@ -1,5 +1,8 @@
 package main.visitor.typeChecker;
 
+import main.ast.nodes.Node;
+import main.ast.nodes.declaration.classDec.ClassDeclaration;
+import main.ast.nodes.declaration.classDec.classMembersDec.MethodDeclaration;
 import main.ast.nodes.expression.*;
 import main.ast.nodes.expression.operators.BinaryOperator;
 import main.ast.nodes.expression.operators.UnaryOperator;
@@ -17,13 +20,12 @@ import main.ast.types.primitives.ClassType;
 import main.ast.types.primitives.IntType;
 import main.ast.types.primitives.VoidType;
 import main.ast.types.set.SetType;
-import main.compileError.typeError.ClassNotDeclared;
-import main.compileError.typeError.ConstructorArgsNotMatchDefinition;
-import main.compileError.typeError.IncDecOperandNotLvalue;
-import main.compileError.typeError.UnsupportedOperandType;
+import main.compileError.typeError.*;
 import main.symbolTable.SymbolTable;
 import main.symbolTable.exceptions.ItemNotFoundException;
 import main.symbolTable.items.ClassSymbolTableItem;
+import main.symbolTable.items.FieldSymbolTableItem;
+import main.symbolTable.items.LocalVariableSymbolTableItem;
 import main.symbolTable.items.MethodSymbolTableItem;
 import main.symbolTable.utils.graph.Graph;
 import main.visitor.Visitor;
@@ -36,6 +38,10 @@ public class ExpressionTypeChecker extends Visitor<Type> {
     public ExpressionTypeChecker(Graph<String> classHierarchy) {
         this.classHierarchy = classHierarchy;
     }
+
+    public ClassDeclaration curr_class;
+
+    public MethodDeclaration curr_method;
 
     public boolean is_subtype_multiple(ArrayList<Type> first, ArrayList<Type> second) {
         if(first.size() != second.size())
@@ -80,6 +86,38 @@ public class ExpressionTypeChecker extends Visitor<Type> {
             return ((ArrayType) first).getType().toString().equals(((ArrayType) second).getType().toString());
         }
         return false;
+    }
+
+    public Boolean is_valid(Type type, Node node) {
+        if(type instanceof ArrayType) {
+            if (!is_valid(((ArrayType) type).getType(), node))
+                return false;
+            ArrayList<Expression> dims = ((ArrayType) type).getDimensions();
+            for (Expression exp : dims) {
+                Type tp = exp.accept(this);
+                if (!(is_subtype(tp, new IntType()))) {
+                    node.addError(new ArrayIndexNotInt(node.getLine()));
+                    return false;
+                }
+            }
+        }
+        if(type instanceof ClassType) {
+            String className = ((ClassType)type).getClassName().getName();
+            if(!this.classHierarchy.doesGraphContainNode(className)) {
+                node.addError(new ClassNotDeclared(node.getLine(), className));
+                return false;
+            }
+        }
+        if(type instanceof FptrType) {
+            Type ret = ((FptrType) type).getReturnType();
+            ArrayList<Type> args = ((FptrType) type).getArgumentsTypes();
+            if (!is_valid(ret, node))
+                return false;
+            for(Type arg : args)
+                if (!is_valid(arg, node))
+                    return false;
+        }
+        return true;
     }
 
     @Override
@@ -193,26 +231,122 @@ public class ExpressionTypeChecker extends Visitor<Type> {
 
     @Override
     public Type visit(MethodCall methodCall) {
-        //Todo
-        return null;
+        Type instance = methodCall.getInstance().accept(this);
+        if (instance instanceof NoType)
+            return new NoType();
+        if (!(instance instanceof FptrType)) {
+            methodCall.addError(new CallOnNoneCallable(methodCall.getLine()));
+            return new NoType();
+        }
+        ArrayList<Type> given_args = new ArrayList<>();
+        for (Expression arg : methodCall.getArgs())
+            given_args.add(arg.accept(this));
+
+        ArrayList<Type> args = ((FptrType) instance).getArgumentsTypes();
+        Type ret = ((FptrType) instance).getReturnType();
+        if (is_subtype_multiple(given_args, args))
+            return ret;
+        methodCall.addError(new MethodCallNotMatchDefinition(methodCall.getLine()));
+        return new NoType();
     }
 
     @Override
     public Type visit(Identifier identifier) {
-        //Todo
-        return null;
+        ClassSymbolTableItem class_item;
+        SymbolTable class_table;
+        MethodSymbolTableItem method_item;
+        SymbolTable method_table;
+        LocalVariableSymbolTableItem local_item;
+        Type tp;
+        try {
+            class_item = (ClassSymbolTableItem) SymbolTable.root.getItem(ClassSymbolTableItem.START_KEY + this.curr_class.getClassName().getName(), true);
+            class_table = class_item.getClassSymbolTable();
+            method_item = (MethodSymbolTableItem) class_table.getItem(MethodSymbolTableItem.START_KEY + this.curr_method.getMethodName().getName(), true);
+            method_table = method_item.getMethodSymbolTable();
+            try {
+                local_item = (LocalVariableSymbolTableItem) method_table.getItem(LocalVariableSymbolTableItem.START_KEY + identifier.getName(), true);
+                tp = local_item.getType();
+                if (is_valid(tp, identifier))
+                    return tp;
+                else
+                    return new NoType();
+            } catch (ItemNotFoundException e) {
+                try {
+                    local_item = (LocalVariableSymbolTableItem) SymbolTable.root.getItem(LocalVariableSymbolTableItem.START_KEY + identifier.getName(), true);
+                    tp = local_item.getType();
+                    if (is_valid(tp, identifier))
+                        return tp;
+                    else
+                        return new NoType();
+                } catch (ItemNotFoundException ignored) {
+                }
+            }
+        } catch (ItemNotFoundException ignored) {
+        }
+        identifier.addError(new VarNotDeclared(identifier.getLine(), identifier.getName()));
+        return new NoType();
     }
 
     @Override
     public Type visit(ArrayAccessByIndex arrayAccessByIndex) {
-        //Todo
-        return null;
+        Expression instance = arrayAccessByIndex.getInstance();
+        Expression index = arrayAccessByIndex.getIndex();
+        Type instance_type = instance.accept(this);
+        Type index_type = index.accept(this);
+        boolean err = false;
+        if (!(instance_type instanceof ArrayType || instance_type instanceof NoType)){
+            arrayAccessByIndex.addError(new AccessByIndexOnNoneArray(arrayAccessByIndex.getLine()));
+            err = true;
+        }
+        if (!is_subtype(index_type, new IntType())){
+            arrayAccessByIndex.addError(new ArrayIndexNotInt(arrayAccessByIndex.getLine()));
+            err = true;
+        }
+        if (err)
+            return new NoType();
+        return instance_type;
     }
 
     @Override
     public Type visit(ObjectMemberAccess objectMemberAccess) {
-        //Todo
-        return null;
+        Expression instance = objectMemberAccess.getInstance();
+        Identifier member_name = objectMemberAccess.getMemberName();
+        Type instance_type = instance.accept(this);
+        if (instance_type instanceof NoType)
+            return new NoType();
+        if (!(instance_type instanceof ClassType)){
+            objectOrListMemberAccess.addError(new AccessOnNonClass(objectMemberAccess.getLine()));
+            return new NoType();
+        }
+        String class_name = ((ClassType) instance_type).getClassName().getName();
+        ClassSymbolTableItem class_item;
+        SymbolTable class_table;
+        try {
+            class_item = (ClassSymbolTableItem) SymbolTable.root.getItem(ClassSymbolTableItem.START_KEY + class_name, true);
+            class_table = class_item.getClassSymbolTable();
+        } catch (ItemNotFoundException e) {
+            return new NoType();
+        }
+        try {
+            FieldSymbolTableItem field_item = (FieldSymbolTableItem) class_table.getItem(FieldSymbolTableItem.START_KEY + member_name.getName(), true);
+            if (is_valid(field_item.getType(), objectMemberAccess))
+                return field_item.getType();
+            else
+                return new NoType();
+        } catch (ItemNotFoundException no_member) {
+            try {
+                MethodSymbolTableItem method_item = (MethodSymbolTableItem) class_table.getItem(MethodSymbolTableItem.START_KEY + member_name.getName(), true);
+                return new FptrType(method_item.getArgTypes(), method_item.getReturnType());
+            } catch (ItemNotFoundException no_method) {
+                if(member_name.getName().equals(class_name)) {
+                    return new FptrType(new ArrayList<>(), new VoidType());
+                }
+                else {
+                    objectMemberAccess.addError(new MemberNotAvailableInClass(objectMemberAccess.getLine(), member_name.getName(), class_name););
+                    return new NoType();
+                }
+            }
+        }
     }
 
     @Override
